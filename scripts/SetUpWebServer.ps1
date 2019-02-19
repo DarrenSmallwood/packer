76584ccb0e1e412781116a24dc4c47dba9bdce28
DarrenSmallwood <DarrenSmallwood@users.noreply.github.com>
2019-02-19 17:16:51 +0000

    Corrected spelling and ignore test results

--- scripts/SetUpWebServer.ps1
+++ scripts/SetUpWebServer.ps1
@@ -1,4 +1,4 @@
-# * Make sure you run this script from a Powershel Admin Prompt!
+# * Make sure you run this script from a Powershell Admin Prompt!
 # * Make sure Powershell Execution Policy is bypassed to run these scripts:
 # * YOU MAY HAVE TO RUN THIS COMMAND PRIOR TO RUNNING THIS SCRIPT!
 Set-ExecutionPolicy Bypass -Scope Process
