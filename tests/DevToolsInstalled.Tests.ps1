30025ff96fd2286271ee0b3e23f8016be4ae1327
DarrenSmallwood <DarrenSmallwood@users.noreply.github.com>
2019-02-19 16:02:56 +0000

    Add webserversetup and amend sysprep

--- tests/DevToolsInstalled.Tests.ps1
+++ tests/DevToolsInstalled.Tests.ps1
@@ -6,4 +6,8 @@ Describe  'Dev Tools installation' {
     It 'installs git' {
         { Get-Command "git" -ErrorAction Stop } | Should Not Throw
     }
+
+    It 'installs web server' {
+        { Get-WindowsFeature -Name web-Server -ErrorAction Stop } | Should Not Throw
+    }
 }
